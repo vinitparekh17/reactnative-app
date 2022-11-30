dcd872db2e1d750300b4f6782f51c8a6dbc113e2
Vinit Parekh <vinitrevaliya@gmail.com>
2022-11-30 23:30:05 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
+    // button on press change background color
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>First app built by Vinit Parekh!</Text>
+      <Text style={styles.text}>Count: {count}</Text>
+      <Button color={'green'} title="Plus" onPress={() => setCount(count+1)} />
+      <Button color={'red'} title="Minus" onPress={() => setCount(count-1)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    color: '#f0f',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    margin: 10,
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: 'red',
+    color: 'white',
+    padding: 10,
+    borderRadius: 5,
+  }
 });
